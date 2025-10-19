2bc328b758264ed441af010482c81cd6ae5c408d
Hossam <alshshtawyfrhwd12@gmail.com>
2025-10-19 12:16:02 -0700

    last_Update_5

--- controllers/BaseController.py
+++ controllers/BaseController.py
@@ -1,19 +1,19 @@
 from helpers.config import get_settings, Settings
-from pathlib import Path
+import os
 import random
 import string
 
 class BaseController:
+    
     def __init__(self):
+
         self.app_settings = get_settings()
-
-        # Get the base directory of the project
-        self.base_dir = Path(__file__).resolve().parents[1]
-
-        # Create a fixed path to the 'assets/files' folder
-        self.files_dir = self.base_dir / "assets" / "files"
-        self.files_dir.mkdir(parents=True, exist_ok=True)  # Ensure the folder exists
-
-    def generate_random_string(self, length: int = 12):
-        # Generate a random lowercase alphanumeric string
+        
+        self.base_dir = os.path.dirname( os.path.dirname(__file__) )
+        self.files_dir = os.path.join(
+            self.base_dir,
+            "assets/files"
+        )
+        
+    def generate_random_string(self, length: int=12):
         return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
